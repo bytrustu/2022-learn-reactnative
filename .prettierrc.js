6dd3887a6c1904bcd1d617caf031a480b529d0e3
bytrustu <bytrustu@gmail.com>
2022-03-11 00:46:31 +0900

    [INIT] 프로젝트 초기 설정

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'singleQuote': true,
+  'parser': 'typescript',
+  'semi': false,
+  'useTabs': false,
+  'tabWidth': 2,
+  'printWidth': 120,
+  'jsxSingleQuote': true,
+  'trailingComma': 'all',
+}
