045f5887a0afa32ed63c1cde494fe1783acdf549
bytrustu <bytrustu@gmail.com>
2022-03-21 01:44:30 +0900

    [ADD] AsyncStorage 적용

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, KeyboardAvoidingView, Platform } from 'react-native'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import DateHead from './components/DateHead'
 import { formatYmd } from './utils/util'
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import AddTodo from './components/AddTodo'
 import Empty from './components/Empty'
 import TodoList from './components/TodoList'
+import todoStorage from './storages/todosStorage'
 
 const App = () => {
 
   const [todos, setTodos] = useState([
     { id: 1, text: '작업환경 설정', done: true },
-    { id: 2, text: '리액트 네이티브 기초 공부', done: false },
+    { id: 2, text: '리액트 네이티브 기초 공부', done: true },
     { id: 3, text: '투두리스트 만들어보기', done: false },
   ])
 
@@ -34,6 +35,18 @@ const App = () => {
     const nextTodos = todos.filter(todo => todo.id !== id)
     setTodos(nextTodos)
   }
+
+  useEffect(() => {
+    todoStorage
+      .get()
+      .then(setTodos)
+      .catch(console.error)
+  }, [])
+
+  useEffect(() => {
+    todoStorage.set(todos).catch(console.error)
+  }, [todos])
+
 
   return (
     <SafeAreaProvider>
